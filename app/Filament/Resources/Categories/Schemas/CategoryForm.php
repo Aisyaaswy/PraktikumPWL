6caeb4332469c63eb08e4eabad0d3221a31cbbae
Aisyaaswy <aisyaaidha@gmail.com>
2026-03-09 23:02:19 +0700

    Migration, Model, Relasi & Resource Category

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make('name')
+                    ->required(),
+
+                    TextInput::make('slug')
+                    ->required()
+                    ->unique(table: 'categories', column: 'slug', ignoreRecord: true),
+            ]);
+    }
+}
