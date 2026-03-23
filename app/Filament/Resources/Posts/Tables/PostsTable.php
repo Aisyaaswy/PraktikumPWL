edde1617cb6d02bddd1fd640ef0ca4997d447048
Aisyaaswy <aisyaaidha@gmail.com>
2026-03-23 21:43:06 +0700

    Search & Filter Table

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -10,6 +10,10 @@ use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostsTable
 {
@@ -18,22 +22,49 @@ class PostsTable
         return $table
             ->columns([
                 //
-                TextColumn::make("title")
-                    ->sortable(),
-                TextColumn::make("slug")
-                    ->sortable(),
-                TextColumn::make("category.name")
-                    ->sortable(),
-                ColorColumn::make("color"),
-                ImageColumn::make("image")
-                ->disk("public"),
-                TextColumn::make("created_at")
+                TextColumn::make('title')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('category.name')
+                    ->sortable()
+                    ->searchable(),
+                ColorColumn::make('color'),
+                ImageColumn::make('image')
+                ->disk('public'),
+                TextColumn::make('created_at')
                     ->Label('Created At')
                     ->dateTime()
                     ->sortable(),
-            ])->defaultSort("created_at", "asc")
+            ])->defaultSort('created_at', 'asc')
             ->filters([
-                //
+                Filter::make('created_at')
+                    ->label('Creation Date Range')
+                    ->form([
+                        DatePicker::make('created_from')
+                            ->label('Dari Tanggal'),
+                        DatePicker::make('created_until')
+                            ->label('Sampai Tanggal'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder { 
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn (Builder $query, $date): Builder 
+                                => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'],
+                                fn (Builder $query, $date): Builder 
+                                => $query->whereDate('created_at', '<=', $date),
+                            );
+                    }),
+                SelectFilter::make('category_id')
+                    ->relationship('category', 'name')
+                    ->label('Category')
+                    ->preload(),
             ])
             ->recordActions([
                 EditAction::make(),
